f1e599b25db638c53c448814429205dc71fa99e3
Cody Rathor <codyrathor@gmail.com>
2018-08-14 10:06:59 -0700

    added more tests for no characters, single character, or character that is not between a-z

--- TDD/tdd-playground/__tests__/string-compression.spec.js
+++ TDD/tdd-playground/__tests__/string-compression.spec.js
@@ -6,22 +6,40 @@ describe('compressString', () => {
       expect(result).toEqual('error')
     })
   })
+  describe('when argument is not empty', () => {
+    it('should return error', () => {
+      const result = compressString()
+      expect(result).toEqual('error')
+    })
+  })
+  describe('when argument length is 1', () => {
+    it('should return character + 1', () => {
+      const result = compressString('a')
+      expect(result).toEqual('a1')
+    })
+  })
+  describe('when argument length is 1 and not between a-z', () => {
+    it('should return error', () => {
+      const result = compressString('!')
+      expect(result).toEqual('error')
+    })
+  })
   describe('when argument is correct type', () => {
     it('should return a compressed string of its letters and the count of each', () => {
       const result = compressString('abbccc')
       expect(result).toEqual('a1b2c3')
     })
   })
-  describe('when argument is correct type but contains numbers', () => {
-    it('should return error', () => {
-      const result = compressString('aab2ccd')
-      expect(result).toEqual('error')
+  describe('when argument is correct type but contains punctuation, numbers or spaces', () => {
+    it('should return compressed string with removed characters', () => {
+      const result = compressString('aab2cc!@_ d')
+      expect(result).toEqual('a2b1c2d1')
     })
   })
   describe('when argument is correct type but contains spaces', () => {
     it('should return compressed string', () => {
       const result = compressString('the fox jumped')
-      expect(result).toEqual('t1h1e2f1o1j1u1m1p1')
+      expect(result).toEqual('t1h1e2f1o1x1j1u1m1p1d1')
     })
   })
 })
--- TDD/tdd-playground/lib/string-compression.js
+++ TDD/tdd-playground/lib/string-compression.js
@@ -1,12 +1,12 @@
 module.exports = str => {
+  if (typeof str !== 'string') return 'error'
   const regex = /\W+|\d|_/g
-
   const charCount = str
     .replace(regex, '')
     .split('')
-    .reduce((total, letter) => {
-      total[letter] ? total[letter]++ : (total[letter] = 1)
-      return total
+    .reduce((acc, letter) => {
+      acc[letter] ? acc[letter]++ : (acc[letter] = 1)
+      return acc
     }, {})
   return Object.keys(charCount)
     .map(char => char + charCount[char])
