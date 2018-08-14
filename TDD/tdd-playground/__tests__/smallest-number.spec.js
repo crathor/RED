044457b43e502d2b34b723f79741455db9b3786c
Cody Rathor <codyrathor@gmail.com>
2018-08-14 16:54:10 -0700

    completed exercise 4. with some help

--- TDD/tdd-playground/__tests__/smallest-number.spec.js
+++ TDD/tdd-playground/__tests__/smallest-number.spec.js
@@ -26,14 +26,50 @@ describe('get smallest number not in an array', () => {
   })
   describe('if array contains all the same number', () => {
     it('should return a number 1 lower than it unless it would equal zero', () => {
-      const result = compress([1, 1, 1, 1, 1])
+      const result = smallestNumber([1, 1, 1, 1, 1])
       expect(result).toEqual(2)
     })
   })
   describe('if array contains valid positive integers', () => {
     it('should return smallest positive number', () => {
-      const result = compress([1, 2, 3, 4, 6, 1])
-      expect(result).toEqual(2)
+      const result = smallestNumber([1, 2, 3, 4, 6, 1])
+      expect(result).toEqual(5)
+    })
+  })
+  describe('when given array', () => {
+    it('should return the lowest number greater than 0 and not in the array', () => {
+      const result = smallestNumber([2, 3, 21, 4])
+      expect(result).toEqual(1)
+    })
+  })
+  describe('when given array with duplicate values', () => {
+    it('should return the lowest number greater than 0 and not in the array', () => {
+      const result = smallestNumber([1, 2, 3, 21, 1, 4, 4])
+      expect(result).toEqual(5)
+    })
+  })
+  describe('when given array with negative numbers', () => {
+    it('should return the lowest number greater than 0 and not in the array', () => {
+      const result = smallestNumber([-1, 3, 21, 4])
+      expect(result).toEqual(1)
+    })
+  })
+  describe('when input is not an array', () => {
+    it('should error', () => {
+      const result = smallestNumber('pickle')
+      expect(result).toEqual(0)
+    })
+  })
+  describe('when given array with non-number indexes', () => {
+    it('should error', () => {
+      const result = smallestNumber([1, 'apple', {}])
+      expect(result).toEqual(0)
+    })
+  })
+  describe('when called without an argument', () => {
+    it('should error', () => {
+      const result = smallestNumber()
+      expect(result).toEqual(0)
     })
   })
 })
--- TDD/tdd-playground/lib/smallestNumber.js
+++ TDD/tdd-playground/lib/smallestNumber.js
@@ -1,1 +1,21 @@
-module.exports = arr => {}
+module.exports = arr => {
+  if (!arr || !arr.length || !Array.isArray(arr)) return 0
+  let result = []
+
+  for (let i = 0; i < arr.length; ++i) {
+    if (typeof arr[i] !== 'number') return 0
+    if (0 <= arr[i]) {
+      result[arr[i]] = true
+    }
+  }
+
+  for (let i = 1; i <= arr.length; ++i) {
+    if (undefined === result[i]) {
+      return i
+    }
+  }
+
+  return 1
+}
+
+// https://codereview.stackexchange.com/questions/179037/given-an-array-of-integers-return-the-smallest-positive-integer-not-in-it
